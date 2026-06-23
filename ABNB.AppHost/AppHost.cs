e2c55857e80e60ecfdbea51b68e67e7945aeef93
diegocalva04 <diegocalva1914@gmail.com>
2026-06-22 20:56:49 -0500

    Eliminr clase innecesaria del dominio, creacion clase Departamento

--- ABNB.AppHost/AppHost.cs
+++ ABNB.AppHost/AppHost.cs
@@ -1,3 +1,13 @@
+using Microsoft.Extensions.Options;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var dbServer = builder.AddPostgres("dbserver").WithLifetime(ContainerLifetime.Persistent);
+dbServer.WithPgAdmin(options => options.WithHostPort(5555).WithLifetime(ContainerLifetime.Persistent)
+);
+
+var db = dbServer.AddDatabase("abnbdb");
+
+builder.AddProject<Projects.Api>("Api").WithExternalHttpEndpoints().WithReference(db).WaitFor(db);
 
 builder.Build().Run();
